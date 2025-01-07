e56bd4b5d584f99b72ce7e9bab42cad611501835
Debanjan <dbjn.ckbrty99@gmail.com>
2025-01-07 14:36:52 +0100

    main.py updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
-from api.routes import router
-from config import settings
+from app.config import settings
+from app.api.routes import router
 
 app = FastAPI(title="Driving Regulations Chatbot")
+
+# Include all your API endpoints from routes.py
 app.include_router(router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "app.main:app",    # "module:variable"
         host="0.0.0.0",
         port=8000,
         reload=settings.debug
